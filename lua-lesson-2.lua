cec6a2eb1dcd94c4a7de616cdc6554c5628e1486
Roman Lazunin <rlazunin@gmail.com>
2017-01-21 18:47:53 +0100

    lua-lesson-2.lua completed

--- lua-lesson-2.lua
+++ lua-lesson-2.lua
@@ -3,4 +3,14 @@
 function find_max( ... )
 	local arguments = { ... }
 	local max = ...
+	for i = 1, #arguments do
+		if arguments[i] > max then
+			max = arguments[i]
+		end
+	end
+
+	return max
 end
+
+answer = find_max(1, 5, 3, 10, 8, 12, 5, 2)
+print(answer)
